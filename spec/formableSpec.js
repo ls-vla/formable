50c5149d94f2c5ed1e8c2d8c35c7afe2d4146e00
vla <vladimir@acid.cl>
2014-03-11 23:00:43 -0400

    Add fieldset specs

--- spec/formableSpec.js
+++ spec/formableSpec.js
@@ -37,25 +37,75 @@ describe("Formable", function() {
     }
   }
 
-
+  // Form Data Tests
   describe("Generated form", function() {
-    var form = new Formable(formJSON, 'output');
+    var $form = new Formable(formJSON, 'output');
 
     it("has a id named 'id' ", function() {
-      expect($(form).attr('id')).toEqual('id');
+      expect($($form).attr('id')).toEqual('id');
     });
 
     it("has a class named 'class' ", function() {
-      expect($(form).attr('class')).toEqual('class');
+      expect($($form).attr('class')).toEqual('class');
     });
 
     it("has a url named '/submit_url' ", function() {
-      expect($(form).attr('url')).toEqual('/submit_url');
+      expect($($form).attr('url')).toEqual('/submit_url');
     });
 
     it("has a legend element with text 'title' ", function() {
-      expect($(form).find('legend').text()).toEqual('title');
-    });  
+      expect($($form).find('legend').text()).toEqual('title');
+    });
+
+    // Fielset Data Tests
+    describe("Fieldset", function() {
+      var $fieldset = $($form).find('fieldset');
+
+      it("contains 2 input elements with type text", function() {
+        expect($($fieldset).find('input[type=text]').length).toEqual(2)
+      });
+
+      it("contains 8 input elements with type radio", function() {
+        expect($($fieldset).find('input[type=radio]').length).toEqual(8)
+      });
+
+      // First Input Text Data
+      describe("First text input", function() {
+        $inputText = $($fieldset).find('input[type=text]').first();
+
+        it("has id with value 'brand'", function() {  
+          expect($($inputText).attr('id')).toEqual('brand');
+        });
+
+        it("has name with value 'brand'", function() {  
+          expect($($inputText).attr('name')).toEqual('brand');
+        });
+
+        it("has class with value 'form-control class'", function() {  
+          expect($($inputText).attr('class')).toEqual('form-control class');
+        });        
+      });
+
+      // First Input Radio Data
+      describe("First radio input", function() {
+        $inputRadio = $($fieldset).find('input[type=radio]').first();
+
+        it("has id with value 'engine'", function() {  
+          expect($($inputRadio).attr('id')).toEqual('engine');
+        });
+
+        it("has name with value 'engine'", function() {  
+          expect($($inputRadio).attr('name')).toEqual('Engine');
+        });
+
+        it("has value equal to 'asdfas'", function() {  
+          expect($($inputRadio).val()).toEqual('1');
+        });
+
+      });
+
+    });
+
   });
   
 
